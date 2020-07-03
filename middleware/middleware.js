396b3158d1de1bdfdeb1358020819cf3e8a61e78
elangathir+1-at-375763065382 <elangathir@neophrontech.com>
2020-07-03 15:02:58 +0530

    mid update

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -10,19 +10,37 @@ server.use(cors({
 	origin: 'http://localhost',
 	// credentials: true
 }));
-server.use("/", (req, res, next) => {
 
-	mongoose.connect(db, {
-		useUnifiedTopology: true,
-		useNewUrlParser: true,
-		useMongoClient:true  
-		});
+server.use("/", (req, res, next) => {
+	try{
+mongoose.connect(db, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useMongoClient:true  
+	});
+}
+catch(err){
+	console.log(err);
+}
+	next();
+})
+
+server.get("/", (req, res) =>	
+	{
+		console.log('test');
+		res.send('working');
+
+})
+
+//server.use("/", (req, res, next) => {
+
+
 
 	//let { protocal, host, port, name } = config.app.db;
 	//mongoose.connect(process.env.MONGODB_URI || `${protocal}${username}:${password}@${host}:${port}/${name}`, { useNewUrlParser: true, useCreateIndex:true, useMongoClient : true});
-	next();
+	//next();
 	// mongodb://user:password123@ds019856.mlab.com:19856/heroku_3xm0068v
-});
+//});
 
 /*server.use(['/contact'], async (req, res, next) => {
 	if(!req.headers.authorization){
